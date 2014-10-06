93a4a6ba1953da20b1804d4b9ec1fb9ccf91bc45
Julio César Rodríguez Domínguez <jurasec@gmail.com>
2014-10-06 14:04:28 -0500

    Sumo jr Robot controlled by keys

---
+++ SoccerKeypress.js
@@ -0,0 +1,80 @@
+var five = require("johnny-five");
+var Spark = require("spark-io");
+var keypress = require("keypress");
+var board = new five.Board({
+  io: new Spark({
+    token: "7550da40ccf9ac697211eb17c031bbe7ac16a20a",
+    deviceId: "53ff72066667574837422067"
+  })
+});
+
+
+board.on("ready", function() {
+  var led = new five.Led("D7");
+  led.blink();
+
+  servo1 = new five.Servo({
+    pin: 'A0',    
+    type: "continuous"
+  });
+
+  servo2 = new five.Servo({
+    pin: 'A4',    
+    type: "continuous"
+  });
+
+  board.repl.inject({
+    servo1: servo1,
+    servo2: servo2
+  });
+
+  /*
+    servo1.cw() => Adelante
+    servo2.cw() => Atrás
+  */
+
+  keypress(process.stdin);
+
+  process.stdin.on('keypress', function (ch, key) {
+    if (key && key.ctrl && key.name == 'c') {
+      process.exit(0);
+    }
+
+    if ( key ){
+      switch ( key.name ){
+        case 'up':
+          console.log('=> Up:');
+          servo1.cw();  
+          servo2.ccw();
+          break;
+        case 'down':          
+          console.log('=> Down:');
+          servo1.ccw();  
+          servo2.cw();
+          break;
+        case 'left':
+          console.log('=> Left:');
+          servo1.ccw();  
+          servo2.ccw();
+          break;
+        case 'right':
+          console.log('=>right');
+          servo1.cw();  
+          servo2.cw();          
+          break;
+        case 's':
+          console.log('=> Stoping...');
+          servo1.stop();
+          servo2.stop();
+          break;
+        case 'e':
+          process.exit(0);
+          break;          
+      }
+    }
+  });
+
+  process.stdin.setRawMode(true);
+  process.stdin.resume();
+
+});
